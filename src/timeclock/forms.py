9a5c8872baaa085e0c40a5552e88d88be70526e3
CFE <hello@teamcfe.com>
2016-11-03 13:22:21 -0700

    12 - Authenticate & Login

--- src/timeclock/forms.py
+++ src/timeclock/forms.py
@@ -4,15 +4,16 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+# http://joincfe.com/projects forms and formsets
+
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
-
     def clean(self, *args, **kwargs):
         cleaned_data = super(LoginForm, self).clean(*args, **kwargs)
-        username = cleaned_data['username']
-        password = cleaned_data['password']
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
         qs = User.objects.filter(username__iexact=username)
         if not qs.exists() or qs.count() != 1:
             raise forms.ValidationError("This username/password is incorrect")
@@ -22,25 +23,23 @@ class LoginForm(forms.Form):
                 raise forms.ValidationError("This username/password is incorrect")
         return cleaned_data
 
+    # def clean_username(self, *args, **kwargs):
+    #     username = self.cleaned_data['username'] #.get('username')
+    #     qs = User.objects.filter(username__iexact=username)
+    #     if not qs.exists() or qs.count() != 1:
+    #         raise forms.ValidationError("This username/password is incorrect")
+    #     return username
 
-
-    def clean_username(self, *args, **kwargs):
-        username = self.cleaned_data['username'] #.get('username')
-        qs = User.objects.filter(username__iexact=username)
-        if not qs.exists() or qs.count() != 1:
-            raise forms.ValidationError("This username/password is incorrect")
-        return username
-
-    def clean_password(self, *args, **kwargs):
-        username = self.cleaned_data['username'] #.get('username')
-        password = self.cleaned_data['password']
-        qs = User.objects.filter(username__iexact=username)
-        if qs.exists() and qs.count() == 1:
-            user_obj = qs.first()
-            if not user_obj.check_password(password):
-                raise forms.ValidationError("This username/password is incorrect")
-            return password
-        else:
-            raise forms.ValidationError("This username/password is incorrect")
+    # def clean_password(self, *args, **kwargs):
+    #     username = self.cleaned_data['username'] #.get('username')
+    #     password = self.cleaned_data['password']
+    #     qs = User.objects.filter(username__iexact=username)
+    #     if qs.exists() and qs.count() == 1:
+    #         user_obj = qs.first()
+    #         if not user_obj.check_password(password):
+    #             raise forms.ValidationError("This username/password is incorrect")
+    #         return password
+    #     else:
+    #         raise forms.ValidationError("This username/password is incorrect")
 
 
--- src/timeclock/views.py
+++ src/timeclock/views.py
@@ -1,26 +1,48 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
 from django.views import View
 
 
+from .forms import LoginForm
 from .models import UserActivity
 
 
 # LOGIN REQUIRED
 class ActivityView(View):
     def get(self, request, *args, **kwargs):
-        # current activity
-        obj = UserActivity.objects.current(request.user)
-        return render(request, "timeclock/activity-view.html", {"object": obj})
+
+        form = LoginForm()
+        context = {
+            "form": form
+        }
+        if request.user.is_authenticated():
+            obj = UserActivity.objects.current(request.user)
+            context['object'] = obj
+        return render(request, "timeclock/activity-view.html", context)
 
     def post(self, request, *args, **kwargs):
-        context = {}
-        if request.user.is_authenticated():
-            toggle = UserActivity.objects.toggle(request.user)
-            context['object'] = toggle
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            #print(form.cleaned_data)
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                request.session['username'] = username
+            return HttpResponseRedirect("/")
+        context = {"form": form}
+        # if request.user.is_authenticated():
+        #     toggle = UserActivity.objects.toggle(request.user)
+        #     context['object'] = toggle
         return render(request, "timeclock/activity-view.html", context)
 
 
+class UserLogoutView(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return HttpResponseRedirect("/")
 
 
 # Django Class Based Views http://joincfe.com/projects
